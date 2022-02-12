317f85de8bf3aaae69b13cbb08f71ecc9acc6d1e
Xuan Zheng <eagledude200@gmail.com>
2022-02-11 19:56:18 -0500

    Update NonTransitiveDice.java

--- Semester 2/Week 4/NonTransitiveDice.java
+++ Semester 2/Week 4/NonTransitiveDice.java
@@ -8,7 +8,44 @@ public class NonTransitiveDice {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
+        int t = readInt();
+        for (int i = 0; i < t; i++) {
+            int[] fDice = new int[4];
+            int[] sDice = new int[4];
+            for (int j = 0; j < 4; j++) {
+                fDice[j] = readInt();
+            }
+            for (int j = 0; j < 4; j++) {
+                sDice[j] = readInt();
+            }
+            Arrays.sort(fDice);
+            Arrays.sort(sDice);
+            for (int a = 1; a <= 10; a++) {
+                for (int b = 1; b <= 10; b++) {
+                    for (int c = 1; c <= 10; c++) {
+                        for (int d = 1; d <= 10; d++) {
+                            // Die C{a,b,c,d};
+                            // if (beats(A,B) && beats(B,C) && beats(C,A)) return 1;
+                            // if (beats(B,A) && beats(C,B) && beats(A,C)) return 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+    public static boolean beats(int[] fDice, int[] sDice) {
+        int wins = 0, losses = 0;
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++) {
+                if (fDice[i] > sDice[j]) {
+                    ++wins;
+                }
+                if (fDice[i] < sDice[j]) {
+                    ++losses;
+                }
+            }
+        return wins > losses;
     }
 
     static String next() throws IOException {
