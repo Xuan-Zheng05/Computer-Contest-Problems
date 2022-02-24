5c5627b22bb77eea1272a8f98376828255df05a1
Xuan Zheng <eagledude200@gmail.com>
2022-02-24 12:25:19 -0500

    Update ArithmeticOrGeometric.java

--- Semester 2/Week 6/ArithmeticOrGeometric.java
+++ Semester 2/Week 6/ArithmeticOrGeometric.java
@@ -1,5 +1,9 @@
 import java.util.*;
 import java.io.*;
+// (a-b)%mod = ((a%mod) - (b%mod))%mod
+// (a+b)%mod = ((a%mod + (b%mod))%mod
+// (a*b)%mod = ((a%mod) * (b%mod))%mod
+// (a/b)%mod ≠ ((a%mod) / (b%mod))%mod
 
 public class ArithmeticOrGeometric {
 
@@ -9,17 +13,36 @@ public class ArithmeticOrGeometric {
 
     public static void main(String[] args) throws IOException {
         int t = readInt();
+        long M = 1000000007;
         for (int i = 0; i < t; i++) {
             int a = readInt(), b = readInt(), c = readInt();
             int k = readInt();
             int commonDifference = b - a;
             if (c - b == commonDifference) {
-                System.out.println(a + (k - 1) * commonDifference);
+                long answer = (a + (k - 1) * commonDifference) % M;
+                System.out.println(answer);
             } else {
                 int commonRatio = b / a;
-                System.out.println(a * Math.pow(commonRatio, k - 1));
+                long answer = factorial(a, commonRatio, k);
+                System.out.println(answer);
+
             }
         }
+    }
+
+    static long factorial(int a, int commonRatio, int k) {
+        long M = 1000000007;
+        long value = a;
+        for (int i = 1; i < k; i++) {
+            if (value > Integer.MAX_VALUE) {
+                value = (value % M * commonRatio) % M;
+            } else {
+                value = (value * commonRatio);
+            }
+
+        }
+
+        return value % M;
     }
 
     static String next() throws IOException {
