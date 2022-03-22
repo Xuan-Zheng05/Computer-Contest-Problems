11436130b8f4d364c837678214792167fbf66619
Xuan Zheng <eagledude200@gmail.com>
2022-03-22 13:38:28 -0400

    Update ObligatoryTriangleProblem.java

--- Semester 2/Week 10/ObligatoryTriangleProblem.java
+++ Semester 2/Week 10/ObligatoryTriangleProblem.java
@@ -8,44 +8,7 @@ public class ObligatoryTriangleProblem {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        int n = readInt();
-        int[] shapes = new int[n];
-        int q = readInt();
-        for (int i = 0; i < q; i++) {
-            String[] query = readLine().split(" ");
-            if (query[0].equals("set")) {
-                if (query[1].equals("square")) {
-                    shapes[Integer.parseInt(query[2]) - 1] = 1;
-                } else if (query[1].equals("circle")) {
-                    shapes[Integer.parseInt(query[2]) - 1] = 2;
-                } else if (query[1].equals("triangle")) {
-                    shapes[Integer.parseInt(query[2]) - 1] = 3;
-                }
-            } else {
-                int shape = shapes[Integer.parseInt(query[2]) - 1];
-                if (query[1].equals("square")) {
-                    if (shape == 1) {
-                        System.out.println(1);
-                    } else {
-                        System.out.println(0);
-                    }
-                }
-                if (query[1].equals("circle")) {
-                    if (shape == 2) {
-                        System.out.println(1);
-                    } else {
-                        System.out.println(0);
-                    }
-                }
-                if (query[1].equals("triangle")) {
-                    if (shape == 3) {
-                        System.out.println(1);
-                    } else {
-                        System.out.println(0);
-                    }
-                }
-            }
-        }
+
     }
 
     static String next() throws IOException {
