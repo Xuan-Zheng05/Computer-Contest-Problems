3fac1a4ea17925b9d0143c7c48736121252d7bd1
Xuan Zheng <eagledude200@gmail.com>
2022-03-25 23:38:43 -0400

    Update ObligatoryPermutationProblem.java

--- Semester 2/Week 10/ObligatoryPermutationProblem.java
+++ Semester 2/Week 10/ObligatoryPermutationProblem.java
@@ -10,8 +10,30 @@ public class ObligatoryPermutationProblem {
     public static void main(String[] args) throws IOException {
         int n = readInt(), k = readInt();
         int[] num = new int[n];
+        // int[] followedNum = new int[n];
         for (int i = 0; i < n; i++) {
             num[i] = readInt();
+        }
+        // for (int i = 0; i < n; i++) {
+        // followedNum[i] = num[num[i] - 1];
+        // System.out.println(followedNum[i]);
+        // }
+
+        int[] num2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            num2[i] = i + 1;
+        }
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                num[j] = num2[num[j] - 1];
+            }
+            System.out.println();
+        }
+        if (k % 2 == 0) {
+            // print num
+        } else {
+            // print num2
         }
     }
 
