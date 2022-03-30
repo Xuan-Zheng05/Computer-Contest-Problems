6e9da0fa23674a267a3e52095e8f66070c2ba9fc
Xuan Zheng <eagledude200@gmail.com>
2022-03-29 21:15:55 -0400

    changes

---
+++ Semester 2/Week 11/ObligatoryStringProblem.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class ObligatoryStringProblem {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        long n = readLong();
+        if (n - 3 < 0) {
+            n = 3;
+        }
+        System.out.println(n - 3);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
