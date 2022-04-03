44c757b766aeb758626b0bb13cc8deeb8cd4cfaf
Xuan Zheng <eagledude200@gmail.com>
2022-04-03 10:06:21 -0400

    upload

---
+++ Semester 2/Quiz/GridDiagonal.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class GridDiagonal {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String[] line1 = readLine().split("");
+        String[] line2 = readLine().split("");
+        String[] line3 = readLine().split("");
+
+        System.out.println(line1[0] + line2[1] + line3[2]);
+        System.out.println(line1[2] + line2[1] + line3[0]);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
