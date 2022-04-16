200b8d0213921102b0290346e9879ffbfbc5a570
Xuan Zheng <eagledude200@gmail.com>
2022-04-16 00:20:41 -0400

    Update ChikaNumbers.java

--- Semester 2/Week 13/ChikaNumbers.java
+++ Semester 2/Week 13/ChikaNumbers.java
@@ -15,19 +15,49 @@ public class ChikaNumbers {
                 grid[i][j] = readInt();
             }
         }
+        boolean possible = true;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[n][m] == 0) {
+                if (grid[i][j] == 0) {
+                    if (i > 0 && j > 0) {
+                        grid[i][j] = Math.max(grid[i - 1][j], grid[i][j - 1]) + 1;
+                    } else if (i > 0) {
+                        grid[i][j] = grid[i - 1][j] + 1;
+                    } else if (j > 0) {
+                        grid[i][j] = grid[i][j - 1] + 1;
+                    } else {
+                        grid[i][j] = 1;
+                    }
 
+                    if (i + 1 < n && j + 1 < m) {
+                        if (grid[i][j] > grid[i + 1][j] && grid[i + 1][j] != 0) {
+                            possible = false;
+                            break;
+                        }
+                        if (grid[i][j] > grid[i][j + 1] && grid[i][j + 1] != 0) {
+                            possible = false;
+                            break;
+                        }
+                    } else if (i + 1 < n) {
+                        if (grid[i][j] > grid[i + 1][j] && grid[i + 1][j] != 0) {
+                            possible = false;
+                            break;
+                        }
+                    } else if (j + 1 < m) {
+                        if (grid[i][j] > grid[i][j + 1] && grid[i][j + 1] != 0) {
+                            possible = false;
+                            break;
+                        }
+                    }
                 }
             }
         }
-        boolean possible = true;
         if (possible) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
-                    System.out.println(grid[i][j]);
+                    System.out.print(grid[i][j] + " ");
                 }
+                System.out.println();
             }
         } else {
             System.out.println(-1);
