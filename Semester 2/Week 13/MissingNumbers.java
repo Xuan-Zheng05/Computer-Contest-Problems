9f1a26175a84210435af30427bfc85d38a5c66bb
Xuan Zheng <eagledude200@gmail.com>
2022-04-15 23:56:20 -0400

    Create MissingNumbers.java

---
+++ Semester 2/Week 13/MissingNumbers.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class MissingNumbers {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int t = readInt();
+        for (int i = 0; i < t; i++) {
+            int pairs = 0;
+            int n = readInt();
+            int newSum = readInt();
+            int oldSum = (int) (((double) n) / 2 * (1 + n));
+            int missing = oldSum - newSum;
+            int mid = missing / 2;
+            if (missing % 2 == 1) {
+                pairs = Math.min(mid, n - mid);
+            } else {
+                pairs = Math.min(mid - 1, n - mid);
+            }
+            System.out.println(pairs);
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
