7ae45dc3f1ea8d00467669b98ac6d4073aec20e5
Xuan Zheng <eagledude200@gmail.com>
2022-04-20 23:34:37 -0400

    Update VictorsMoralDilemma.java

--- Semester 2/Week 14/VictorsMoralDilemma.java
+++ Semester 2/Week 14/VictorsMoralDilemma.java
@@ -9,47 +9,24 @@ public class VictorsMoralDilemma {
 
     public static void main(String[] args) throws IOException {
         int n = readInt(), d = readInt();
-        int[] nums = new int[n + 1];
-        ArrayList<Integer> psa = new ArrayList<>();
-        psa.add(0);
+
+        int[] psa = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            nums[i] = readInt();
-            psa.add(psa.get(i - 1) + nums[i]);
+            psa[i] = psa[i - 1] + readInt();
         }
+
         int bottom = 0, top = n;
         for (int i = 0; i < d; i++) {
             int split = readInt();
-            if (psa.get(split) >= psa.get(top) - psa.get(split)) {
-                System.out.println(psa.get(split));
-                psa.clear();
-
+            if (psa[split + bottom] - psa[bottom] >= psa[top] - (psa[split] -
+                    psa[bottom])) {
+                System.out.println(psa[split + bottom] - psa[bottom]);
+                bottom = split;
             } else {
-                System.out.println(psa.get(top) - psa.get(split));
-                psa.clear();
-                psa.add(0);
-                for (int j = 1 + bottom; j <= split + bottom; j++) {
-                    psa.add(nums[j] + psa.get(j - 1));
-                }
+                System.out.println(psa[top] - psa[split + bottom]);
                 top = split;
             }
         }
-        // int[] psa = new int[n + 1];
-        // for (int i = 1; i <= n; i++) {
-        // psa[i] = psa[i - 1] + readInt();
-        // }
-
-        // int bottom = 0, top = n;
-        // for (int i = 0; i < d; i++) {
-        // int split = readInt();
-        // if (psa[split + bottom] - psa[bottom] >= psa[top] - (psa[split] -
-        // psa[bottom])) {
-        // System.out.println(psa[split + bottom] - psa[bottom]);
-        // bottom = split;
-        // } else {
-        // System.out.println(psa[top] - psa[split + bottom]);
-        // top = split;
-        // }
-        // }
     }
 
     static String next() throws IOException {
