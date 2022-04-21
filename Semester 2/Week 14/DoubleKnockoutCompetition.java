94f77b3f5e3bd001dba3f7f1b6946c8da1daf86a
Xuan Zheng <eagledude200@gmail.com>
2022-04-21 13:43:51 -0400

    Update DoubleKnockoutCompetition.java

--- Semester 2/Week 14/DoubleKnockoutCompetition.java
+++ Semester 2/Week 14/DoubleKnockoutCompetition.java
@@ -11,19 +11,41 @@ public class DoubleKnockoutCompetition {
         int n = readInt();
         for (int i = 0; i < n; i++) {
             int teams = readInt();
+            System.out.println(teams);
             boolean flag = true;
             int zeroLosses = teams;
-            int oneLosses = zeroLosses / 2;
-            int round = 1;
+            int oneLosses = 0;
+            int round = 0;
+            int eliminated = 0;
+            System.out.println(
+                    "Round " + round + ": " + zeroLosses + " undefeated, " + oneLosses + " one-loss, " + eliminated
+                            + " eliminated");
+            round++;
             while (flag) {
-
+                eliminated = oneLosses / 2;
+                oneLosses /= 2;
                 oneLosses += zeroLosses / 2;
+                zeroLosses /= 2;
+                if (zeroLosses <= 0) {
+                    zeroLosses = 1;
+                }
                 if (oneLosses == 1 && zeroLosses == 1) {
-                    System.out.println("Round 3: 0 undefeated, 1 one-loss, 1 eliminated");
+                    System.out.println("Round " + round + ": 1 undefeated, 1 one-loss, " + eliminated + " eliminated");
+                    round++;
+                    System.out.println("Round " + round + ": 0 undefeated, 2 one-loss, 0 eliminated");
+                    round++;
+                    System.out.println("Round " + round + ": 0 undefeated, 1 one-loss, 1 eliminated");
+                    System.out.println("There are " + round + " rounds.");
+                    break;
 
+                } else {
+                    System.out.println("Round " + round + ": " + zeroLosses + " undefeated, " + oneLosses
+                            + " one-loss, " + eliminated
+                            + " eliminated");
                 }
                 round++;
             }
+            System.out.println();
         }
     }
 
