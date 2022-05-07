7b3ad919dfd7b0feca20e2a337b6be79ab16d5b7
Xuan Zheng <eagledude200@gmail.com>
2022-05-06 23:47:15 -0400

    Update Nizovi.java

--- Semester 2/Wekk 16/Nizovi.java
+++ Semester 2/Wekk 16/Nizovi.java
@@ -8,7 +8,48 @@ public class Nizovi {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-
+        char[] line = readLine().toCharArray();
+        int numIndent = 0;
+        for (int i = 0; i < line.length; i++) {
+            if (line[i] == '{') {
+                if (line[i + 1] == '}') {
+                    i++;
+                    String indent = "";
+                    for (int j = 0; j < numIndent; j++) {
+                        indent += " ";
+                    }
+                    System.out.println("{");
+                    System.out.print(indent);
+                    System.out.print("}");
+                } else {
+                    System.out.println("{");
+                    numIndent += 2;
+                    String indent = "";
+                    for (int j = 0; j < numIndent; j++) {
+                        indent += " ";
+                    }
+                    System.out.print(indent);
+                }
+            } else if (line[i] == '}') {
+                System.out.println();
+                numIndent -= 2;
+                String indent = "";
+                for (int j = 0; j < numIndent; j++) {
+                    indent += " ";
+                }
+                System.out.print(indent + "}");
+            } else if (line[i] == ',') {
+                String indent = "";
+                for (int j = 0; j < numIndent; j++) {
+                    indent += " ";
+                }
+                System.out.println(",");
+                System.out.print(indent);
+            } else {
+                System.out.print(line[i]);
+            }
+        }
+        System.out.println();
     }
 
     static String next() throws IOException {
