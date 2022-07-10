1fa318d62fd7a63f877ea1c2ed7ed030edead764
Xuan Zheng <eagledude200@gmail.com>
2022-07-09 23:36:16 -0400

    Update ChampionContest.java

--- Semester 3/Class 1 Problems/ChampionContest.java
+++ Semester 3/Class 1 Problems/ChampionContest.java
@@ -8,6 +8,53 @@ public class ChampionContest {
 
     public static void main(String[] args) throws IOException {
         int n = readInt(), k = readInt();
+        int[] champs = new int[n];
+        int[] sortedChamps = new int[n];
+        int[] pair1 = new int[k];
+        int[] pair2 = new int[k];
+
+        for (int i = 0; i < n; i++) {
+            champs[i] = readInt();
+            sortedChamps[i] = champs[i];
+        }
+
+        for (int i = 0; i < k; i++) {
+            pair1[i] = readInt();
+            pair2[i] = readInt();
+        }
+
+        Arrays.sort(sortedChamps);
+
+        for (int i = 0; i < n; i++) {
+            int cantFight = 0;
+            for (int j = 0; j < k; j++) {
+                if (pair1[j] == i + 1) {
+                    if (champs[i] > champs[pair2[j] - 1]) {
+                        cantFight++;
+                    }
+                } else if (pair2[j] == i + 1) {
+                    if (champs[i] > champs[pair1[j] - 1]) {
+                        cantFight++;
+                    }
+                }
+            }
+
+            int index = Arrays.binarySearch(sortedChamps, champs[i]);
+
+            if (index > 0) {
+                int counter = index - 1;
+                while (counter >= 0) {
+                    if (sortedChamps[index] != sortedChamps[counter]) {
+                        break;
+                    } else {
+                        index--;
+                        counter--;
+                    }
+                }
+            }
+
+            System.out.print((index - cantFight) + " ");
+        }
 
     }
 
