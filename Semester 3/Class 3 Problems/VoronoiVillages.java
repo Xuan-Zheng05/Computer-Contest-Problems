a5fa629b1044350ad4277fc771650892400e0178
Xuan Zheng <eagledude200@gmail.com>
2022-07-23 00:36:40 -0400

    Update VoronoiVillages.java

--- Semester 3/Class 3 Problems/VoronoiVillages.java
+++ Semester 3/Class 3 Problems/VoronoiVillages.java
@@ -8,7 +8,19 @@ public class VoronoiVillages {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
+        int n = readInt();
+        double[] villages = new double[n];
+        for (int i = 0; i < n; i++) {
+            villages[i] = readInt();
+        }
+        Arrays.sort(villages);
+        ArrayList<Double> distance = new ArrayList<>();
+        for (int i = 1; i < n - 1; i++) {
+            distance.add((villages[i + 1] - villages[i]) / 2 + (villages[i] - villages[i - 1]) / 2);
+        }
 
+        Collections.sort(distance);
+        System.out.println(distance.get(0));
     }
 
     static String next() throws IOException {
