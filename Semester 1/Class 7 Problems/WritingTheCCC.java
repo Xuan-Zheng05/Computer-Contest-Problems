c0ac3bbe10930bcab8e7ab0d936902742116ad58
Xuan Zheng <eagledude200@gmail.com>
2022-07-22 22:04:51 -0400

    upload

--- Semester 1/Class 7 Problems/WritingTheCCC.java
+++ Semester 1/Class 7 Problems/WritingTheCCC.java
@@ -1,30 +1,30 @@
 import java.util.*;
 import java.io.*;
-   
-public class WritingTheCCC { 
-   
-   static BufferedReader br = getBufferedReader(true);
+
+public class WritingTheCCC {
+
+   static BufferedReader br = getBufferedReader(false);
    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
    static StringTokenizer st;
-    
+
    public static void main(String[] args) throws IOException {
-      
+
       Map<String, ArrayList> map = new HashMap<String, ArrayList>();
-      int t = readInt();  
-      String[] types = new String[t];      
+      int t = readInt();
+      String[] types = new String[t];
       for (int i = 0; i < t; i++) {
          String type = readLine();
          types[i] = type;
          map.put(type, new ArrayList());
       }
-      
-      int n = readInt();           
+
+      int n = readInt();
       for (int i = 0; i < n; i++) {
          String problem = readLine();
          List<Integer> tempList = map.get(problem);
-         tempList.add(i+1);
+         tempList.add(i + 1);
       }
-      
+
       for (int i = 0; i < t; i++) {
          List<Integer> tempList = map.get(types[i]);
          for (int j = 0; j < tempList.size(); j++) {
@@ -32,37 +32,43 @@ public class WritingTheCCC {
          }
       }
    }
-   
-   static String next () throws IOException {
+
+   static String next() throws IOException {
       while (st == null || !st.hasMoreTokens())
          st = new StringTokenizer(br.readLine().trim());
       return st.nextToken();
    }
-   static long readLong () throws IOException {
+
+   static long readLong() throws IOException {
       return Long.parseLong(next());
    }
-   static int readInt () throws IOException {
+
+   static int readInt() throws IOException {
       return Integer.parseInt(next());
    }
-   static double readDouble () throws IOException {
+
+   static double readDouble() throws IOException {
       return Double.parseDouble(next());
    }
-   static char readCharacter () throws IOException {
+
+   static char readCharacter() throws IOException {
       return next().charAt(0);
    }
-   static String readLine () throws IOException {
+
+   static String readLine() throws IOException {
       return br.readLine().trim();
    }
-   static BufferedReader getBufferedReader (boolean fromFile) {
-      try {        
+
+   static BufferedReader getBufferedReader(boolean fromFile) {
+      try {
          if (fromFile) {
             br = new BufferedReader(new FileReader("test.txt"));
          } else {
             br = new BufferedReader(new InputStreamReader(System.in));
-         }   
-         
-         return br; 
-            
+         }
+
+         return br;
+
       } catch (Exception e) {
          e.printStackTrace();
          return null;
