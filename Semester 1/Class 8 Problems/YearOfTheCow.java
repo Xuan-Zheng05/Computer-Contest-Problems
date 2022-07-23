cc0298fdfa13574be9d90ec1c93842e90a3cad98
Xuan Zheng <eagledude200@gmail.com>
2022-07-23 01:19:18 -0400

    upload

--- Semester 1/Class 8 Problems/YearOfTheCow.java
+++ Semester 1/Class 8 Problems/YearOfTheCow.java
@@ -1,108 +1,104 @@
 import java.util.*;
 import java.io.*;
-   
-public class YearOfTheCow { 
-   
+
+public class YearOfTheCow {
+
    static BufferedReader br = getBufferedReader(true);
    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
    static StringTokenizer st;
-    
+
    public static void main(String[] args) throws IOException {
-    
+
       Map<String, Integer> year = new HashMap<String, Integer>();
-      Map<String, Integer> rel = new HashMap<String, Integer>();   
-      Map<String, Integer> zind = new HashMap<String, Integer>();  
-      Map<String, Integer> borns = new HashMap<String, Integer>();     
-      
-      String[] calender = {"Ox", "Tiger", "Rabbit", "Dragon", "Snake", 
-         "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig", "Rat"};
-      
+      Map<String, Integer> rel = new HashMap<String, Integer>();
+      Map<String, Integer> zind = new HashMap<String, Integer>();
+      Map<String, Integer> borns = new HashMap<String, Integer>();
+
+      String[] calender = { "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+            "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig", "Rat" };
+
       for (int i = 0; i < 12; i++) {
          zind.put(calender[i], i);
       }
-      
-      borns.put("Bessie",0);
-      
-      int n = readInt();     
-      int d =0;
-   //Mildred born in previous Dragon year from Bessie
+      borns.put("Bessie", 0);
+      int n = readInt();
+      int d = 0;
       for (int i = 0; i < n; i++) {
          String[] sentence = readLine().split(" ");
-         
-         int a, b,  years;
+
+         int a, b, years;
          boolean previous;
          String nameA = sentence[0];
          String nameB = sentence[7];
-         previous = sentence[3].equals("previous") ? true:false;
-         
+         previous = sentence[3].equals("previous") ? true : false;
+
          int yearA = zind.get(sentence[4]);
          int person = borns.get(nameB);
          if (previous) {
             int diff;
-            if(person >yearA)
+            if (person > yearA)
                diff = person - yearA;
             else
                diff = 12 + person - yearA;
-            if(nameB.equals("Bessie")) 
-               d =0;
+            if (nameB.equals("Bessie"))
+               d = 0;
             d -= diff;
-          
-           //  year.put(nameA, yearA);
-         //             rel.put(nameA, rel.get(nameB) - diff);           
-         } 
-         
+         }
+
          else {
             int diff;
-            if(person <yearA){
+            if (person < yearA) {
                diff = yearA - person;
-            }else{
+            } else {
                diff = 12 - person + yearA;
             }
-            if(nameB.equals("Bessie")) 
-               d =0;
-            d +=diff;
+            if (nameB.equals("Bessie"))
+               d = 0;
+            d += diff;
          }
-         borns.put(nameA,yearA);
-          
-         
-      
-          
+         borns.put(nameA, yearA);
          if (nameA.equals("Elsie"))
             break;
       }
-      System.out.println(Math.abs(d) );
+      System.out.println(Math.abs(d));
    }
-   
-   static String next () throws IOException {
+
+   static String next() throws IOException {
       while (st == null || !st.hasMoreTokens())
          st = new StringTokenizer(br.readLine().trim());
       return st.nextToken();
    }
-   static long readLong () throws IOException {
+
+   static long readLong() throws IOException {
       return Long.parseLong(next());
    }
-   static int readInt () throws IOException {
+
+   static int readInt() throws IOException {
       return Integer.parseInt(next());
    }
-   static double readDouble () throws IOException {
+
+   static double readDouble() throws IOException {
       return Double.parseDouble(next());
    }
-   static char readCharacter () throws IOException {
+
+   static char readCharacter() throws IOException {
       return next().charAt(0);
    }
-   static String readLine () throws IOException {
+
+   static String readLine() throws IOException {
       return br.readLine().trim();
    }
-   static BufferedReader getBufferedReader (boolean fromFile) {
-      try {        
+
+   static BufferedReader getBufferedReader(boolean fromFile) {
+      try {
          if (fromFile) {
             br = new BufferedReader(new FileReader("test.txt"));
          } else {
             br = new BufferedReader(new InputStreamReader(System.in));
-         }   
-         
+         }
+
          return br;
-            
+
       } catch (Exception e) {
          e.printStackTrace();
          return null;
--- Semester 3/Class 3 Problems/MediaList.java
+++ Semester 3/Class 3 Problems/MediaList.java
@@ -8,7 +8,41 @@ public class MediaList {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
+        int n = readInt(), q = readInt();
+        Map<String, ArrayList<Integer>> map = new HashMap<>();
 
+        for (int i = 0; i < q; i++) {
+
+            String[] line = readLine().split(" ");
+            int command = Integer.parseInt(line[0]);
+            int list = Integer.parseInt(line[1]);
+            String name = line[2];
+
+            if (command == 1) {
+                if (map.containsKey(name)) {
+                    ArrayList<Integer> temp = map.get(name);
+                    if (temp.contains(list)) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                } else {
+                    System.out.println(0);
+                }
+            } else {
+                if (map.containsKey(name)) {
+                    ArrayList<Integer> temp = map.get(name);
+                    if (!temp.contains(list)) {
+                        temp.add(list);
+                        map.put(name, temp);
+                    }
+                } else {
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(list);
+                    map.put(name, temp);
+                }
+            }
+        }
     }
 
     static String next() throws IOException {
